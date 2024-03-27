34dd126ab2671da9793939f15d2fff1b010c48d3
AndoniUTT <andoni2020lopez@gmail.com>
2024-03-27 11:13:12 -0600

    Agregacion de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importa la librería de Firebase
 import 'package:homanager_app/main_app.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Inicializa Firebase con las opciones predeterminadas
   runApp(const MainApp());
 }
