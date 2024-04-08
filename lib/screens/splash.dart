8b4a7c19d03a068c5cad117f17772a46665085a5
AndoniUTT <andoni2020lopez@gmail.com>
2024-04-08 10:57:00 -0600

    implementacion - login

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:homanager_app/screens/login.dart';
+import 'package:homanager_app/screens/login copy.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key});
